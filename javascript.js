e16ca8b1be044693223ec9303170f666f08eaf84
Winkelton <148702867+Winkelton@users.noreply.github.com>
2023-12-04 19:16:45 +0100

    Reworked Code for the new RPS-UI

--- javascript.js
+++ javascript.js
@@ -1,6 +1,91 @@
-function FixInput(Input){
-   return Input[0].toUpperCase() + Input.slice(1).toLowerCase();
-}
+let GameRounds = 0;
+let PlayerWins = 0;
+let ComputerWins = 0;
+let Draws = 0;
+const content = document.querySelector("#container");
+const rockbtn = document.createElement("button");
+const paperbtn = document.createElement("button");
+const scissorsbtn = document.createElement("button");
+const results = document.createElement("div");
+const startText = document.createElement("span");
+const GameRoundsText = document.createElement("span");
+rockbtn.setAttribute("id","Rock");
+paperbtn.setAttribute("id","Paper");
+scissorsbtn.setAttribute("id","Scissors");
+startText.textContent = "Select either Rock, Paper or Scissors:";
+startText.classList.add("startText");
+results.classList.add("results");
+results.setAttribute("id","results");
+rockbtn.textContent = "Rock";
+paperbtn.textContent = "Paper";
+scissorsbtn.textContent = "Scissors";
+const WinnerAnnouncement = document.createElement("span");
+const playerWinstext = document.createElement("span");
+const computerWinstext = document.createElement("span");
+const Drawstext = document.createElement("span");
+WinnerAnnouncement.setAttribute("id","WinnerAnnouncement");
+content.append(startText,rockbtn,paperbtn,scissorsbtn,results,WinnerAnnouncement);
+results.append(playerWinstext,computerWinstext,Drawstext,GameRoundsText)
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+    switch(playRound(e.target.id)){
+        case "Computer":
+            if(GameRounds <5){
+            GameRounds +=1;
+            ComputerWins +=1;
+            computerWinstext.textContent = "Computer Wins: "+ComputerWins;
+            GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+            } else {
+                computerWinstext.textContent = "Computer Wins: "+ComputerWins;
+                GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+                if (ComputerWins > PlayerWins){
+                    WinnerAnnouncement.textContent = "The Winner is: Computer";
+                } else {
+                    WinnerAnnouncement.textContent = "The Winner is: Player";
+                }
+            }
+        break;
+        case "Player":
+            if(GameRounds <5){
+            GameRounds +=1;
+            PlayerWins +=1;
+            playerWinstext.textContent = "Player: "+PlayerWins;
+            GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+        } else {
+            computerWinstext.textContent = "Computer Wins: "+ComputerWins;
+            GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+            if (ComputerWins > PlayerWins){
+                WinnerAnnouncement.textContent = "The Winner is: Computer";
+            } else {
+                WinnerAnnouncement.textContent = "The Winner is: Player";
+            }
+        }
+        break;
+        case "Draw":
+            if(GameRounds <5){
+            GameRounds +=1;
+            Draws +=1;
+            Drawstext.textContent = "Draws: "+Draws;
+            GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+        } else {
+            Drawstext.textContent = "Draws: "+Draws;
+            GameRoundsText.textContent = "Game Rounds: "+GameRounds;
+            if (ComputerWins > PlayerWins){
+                WinnerAnnouncement.textContent = "The Winner is: Computer";
+            } else {
+                WinnerAnnouncement.textContent = "The Winner is: Player";
+            }
+        }
+        break;
+
+    }
+ 
+    });
+});
+
+
+
 
 function getComputerChoice(){
     let Choice =  Math.floor(Math.random() * 3);
@@ -15,74 +100,53 @@ function getComputerChoice(){
         return computerPull;
     }
 }
-function playRound(){
+function playRound(playerSelection){
+// 0 = draw, 1 = player win and 2 = computer win
 
-    let decision = prompt("Rock Paper or Scissors?");
-    let Player = FixInput(decision);
     let Computer = getComputerChoice();
-    
     let winner = 0;
-    switch (Player){
+    switch (playerSelection){
         case "Rock":
             if (Computer == "Rock"){
-                winner = 0;
-                return  winner;
+                winner = "Draw";
+                
             } else if (Computer == "Paper"){
-                winner = 2;
-                return  winner;
+                winner = "Computer";
+                
             } else if (Computer == "Scissors"){
-                winner = 1;
-                return winner;
+                winner = "Player";
+                
             }
             break;
         case "Paper":
             if (Computer == "Paper"){
-                winner = 0;
-                return  winner;
+                winner = "Draw";
+              
             } else if (Computer == "Scissors"){
-                winner = 2;
-                return winner;
+                winner = "Computer";
+              
             } else if (Computer == "Rock"){
-                winner = 1;
-                return winner;
+                winner = "Player";
+               
             }
             break;
             case "Scissors":
             if (Computer == "Scissors"){
-                winner = 0;
-                return winner;
+                winner = "Draw";
+                
             } else if (Computer == "Rock"){
-                winner = 2;
-                return winner;
+                winner = "Computer";
+                
             } else if (Computer == "Paper"){
-                winner = 1;
-                return winner;
+                winner = "Player";
+            
             }
             break;
     }
-    return winner;
+  
+    
+   
+    return  winner;
+
 }
 
-function game(){
-    let playerWins = 0;
-    let computerWins = 0;
-    let draws = 0;
-    
-    for (i=1;i<=5;i++){
-        let game = playRound();
-        if (game == 0){
-            draws +=1;
-        } else if (game == 1){
-            playerWins +=1;
-        } else {
-            computerWins +=1;
-        }
-    }
-    if (playerWins > computerWins){
-        winner = "Player";
-    } else {
-        winner = "Computer";
-    }
-   alert("You played 5 Rounds, you won "+playerWins+" times, the computer won "+computerWins+" times and you got "+draws+" Draws!"+"\n The winner is: "+winner)
-}
-game();
